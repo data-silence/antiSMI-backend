dcdfca77cfffe636f031c30c953733b431b76cf0
data-silence <enjoy-ds@pm.me>
2024-03-12 15:59:43 +0300

    Improve get_similar_news router

--- app/news/dao.py
+++ app/news/dao.py
@@ -44,10 +44,12 @@ class NewsDao(BaseDao):
             return result.mappings().all()
 
     @staticmethod
-    async def get_similar_news(embedding: list[float]):
+    async def get_similar_news(embedding: list[float], start_date: date, end_date: date, news_amount: int,
+                               category: str):
         async with tm_async_session_maker() as session:
-            query = select(News.__table__.columns, Embs.embedding).join(News, Embs.news_url == News.url).order_by(
-                Embs.embedding.l2_distance(embedding)).limit(10)
+            query = select(News.__table__.columns, Embs.embedding).join(News, Embs.news_url == News.url).where(
+                and_(start_date <= News.date, News.date <= end_date)).filter(News.category == category).order_by(
+                Embs.embedding.l2_distance(embedding)).limit(news_amount)
             result = await session.execute(query)
             return result.mappings().all()
 
